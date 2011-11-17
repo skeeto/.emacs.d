54dc9144abf8218b239271ab2ad38bde66e1c6f5
Christopher Wellons <mosquitopsu@gmail.com>
2011-11-17 08:56:02 -0500

    Fix markdown-mode for YASnippet.

--- init.el
+++ init.el
@@ -47,6 +47,7 @@
 (require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(define-key markdown-mode-map (kbd "<tab>") nil) ; fix for YASnippet
 
 ;; Printing
 (require 'ps-print)
