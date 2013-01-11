0e4e7aefaca7aedc2c14de6996d220884816bf3c
Christopher Wellons <mosquitopsu@gmail.com>
2013-01-11 00:37:08 -0500

    Workaround nrepl bugs.

--- init.el
+++ init.el
@@ -153,6 +153,8 @@
 ;; Clojure
 (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
 
+; Fix nrepl.el's broken error buffer
+(defalias 'nrepl-popup-buffer-quit-function 'quit-window)
 (defadvice nrepl-default-err-handler (after nrepl-focus-errors activate)
   "Focus the error buffer after errors, like Emacs normally does."
   (select-window (get-buffer-window "*nrepl-error*")))
