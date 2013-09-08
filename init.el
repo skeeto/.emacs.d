a277307b816eeb9de46dd83bb17ed46052e9afbd
Christopher Wellons <wellons@nullprogram.com>
2013-09-08 12:59:38 -0400

    Only run package-initialize once.

--- init.el
+++ init.el
@@ -31,6 +31,7 @@
 
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(setq package-enable-at-startup nil)
 (package-initialize)
 
 ;; Install packages not needing configuration
