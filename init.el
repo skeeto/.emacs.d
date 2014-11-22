4f406f96082267c2c6a0b52c67f5026fe8945595
Christopher Wellons <wellons@nullprogram.com>
2014-11-22 13:27:02 -0500

    Adjust scrolling.

--- init.el
+++ init.el
@@ -26,6 +26,10 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (prefer-coding-system 'utf-8-unix)
 
+;; Adjust scrolling
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
+(setq scroll-step 1)
+
 ;;; Packages
 
 (require 'package)
