32517c37704a2d4a77a94e138b5e41fb77be8dff
Christopher Wellons <wellons@nullprogram.com>
2015-03-22 10:29:43 -0400

    Remove an old feed.

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -92,7 +92,6 @@
   ("http://echosa.github.io/atom.xml" blog dev)
   ("http://www.devrand.org/feeds/posts/default" blog dev)
   ("http://random.terminally-incoherent.com/rss" image)
-  ("https://glyph.twistedmatrix.com/feeds/atom.xml" blog dev)
   ("http://crawl.develz.org/wordpress/feed" blog gaming product)
   ("http://dvdp.tumblr.com/rss" image)
   ("https://www.digitalocean.com/blog/feed" blog product)
