0758614b698fce0629645c1bbe6b0eae24cf1b0d
Christopher Wellons <wellons@nullprogram.com>
2015-07-30 17:34:44 -0400

    Add sudo-edit.

--- lisp/extras.el
+++ lisp/extras.el
@@ -255,6 +255,17 @@ Non-null optional arg FILES-ONLY means mention only file buffers."
   (interactive "P")
   (display-buffer (list-buffers-noselect files-only (buffer-list))))
 
+(defun sudo-edit (&optional arg)
+  "Edit currently visited file as root.
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+  (interactive "P")
+  (if (or arg (not buffer-file-name))
+      (find-file (concat "/sudo:root@localhost:"
+                         (ido-read-file-name "Find file(as root): ")))
+    (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
+
 (provide 'extras)
 
 ;;; extras.el ends here
