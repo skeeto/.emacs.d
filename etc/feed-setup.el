d7361a54d054ac7d0786cb0e5b25014282957ac9
Christopher Wellons <wellons@nullprogram.com>
2016-03-05 09:45:24 -0500

    Disable problematic feed (Emacs#20465).
    Emacs 24 and below don't send Server Name Indication (SNI) in the
    request headers, causing feeds on shared hosts employing HTTPS, such as
    CloudFlare, to fail with code gnutls error -12.
    
    https://lists.gnu.org/archive/html/bug-gnu-emacs/2015-04/msg01157.html
    
    I will re-enable this feed when I upgrade to the upcoming Emacs 25.

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -185,7 +185,7 @@
   ("http://www.safelyendangered.com/feed/" comic)
   ("https://www.schneier.com/blog/atom.xml" blog security)
   ("http://www.smbc-comics.com/rss.php" comic)
-  ("https://haneefmubarak.com/rss/" dev blog)
+  ;;("https://haneefmubarak.com/rss/" dev blog) ; Emacs bug #20465
   ("http://feeds.feedburner.com/spaceavalanche1" comic)
   ("http://www.howstuffworks.com/podcasts/stuff-you-should-know.rss" audio)
   ("http://thecodelesscode.com/rss" dev story)
