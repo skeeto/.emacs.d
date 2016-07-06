c42a30bf0c132a5e9e03a917da460b9506a0f60d
Christopher Wellons <wellons@nullprogram.com>
2016-07-06 08:22:49 -0400

    elfeed: Add David Friedman's blog.

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -169,6 +169,7 @@
   ("http://feeds.feedburner.com/channelATE" comic)
   ("http://deep-dark-fears.tumblr.com/rss" comic)
   ("http://crawl.develz.org/wordpress/feed" blog gaming product)
+  ("https://www.blogger.com/feeds/19727420/posts/default" blog)
   ("https://www.debian.org/security/dsa" debian list security important)
   ("http://dvdp.tumblr.com/rss" image)
   ("https://www.digitalocean.com/blog/feed" blog product)
