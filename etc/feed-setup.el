1c9f9380859759806461cf9cfeb7d831b16877b6
Christopher Wellons <wellons@nullprogram.com>
2016-11-28 07:16:37 -0500

    Oops, fix slow youtube-dl key binding.

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -71,8 +71,8 @@
       (browse-url (replace-regexp-in-string "/watch" "/all_comments" url)))))
 
 (define-key elfeed-show-mode-map "d" 'elfeed-show-youtube-dl)
-(define-key elfeed-show-mode-map "D" 'elfeed-show-youtube-dl-slow)
 (define-key elfeed-search-mode-map "d" 'elfeed-search-youtube-dl)
+(define-key elfeed-search-mode-map "D" 'elfeed-search-youtube-dl-slow)
 (define-key elfeed-search-mode-map "L" 'youtube-dl-list)
 (define-key elfeed-search-mode-map "c" 'elfeed-search-youtube-comments)
 
