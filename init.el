619f943a6f8fc7aa19a9522ff15accf8dc7c4217
Christopher Wellons <wellons@nullprogram.com>
2017-04-21 15:05:36 -0400

    Initially put commit buffer in insert mode.

--- init.el
+++ init.el
@@ -183,6 +183,7 @@
   (add-to-list 'evil-emacs-state-modes 'special-mode)
   (add-to-list 'evil-emacs-state-modes 'youtube-dl-list-mode)
   (evil-select-search-module 'evil-search-module 'evil-search)
+  (add-hook 'with-editor-mode-hook 'evil-insert-state)
   (add-hook 'emacs-lisp-mode-hook (lambda () (modify-syntax-entry ?- "w")))
   (add-hook 'c-mode-common-hook (lambda () (modify-syntax-entry ?_ "w"))))
 
