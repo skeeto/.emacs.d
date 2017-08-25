8f641c01139237287c3aa9b68c35a80e996bcb42
Christopher Wellons <wellons@nullprogram.com>
2017-08-25 14:55:47 -0400

    Add ctags-find-reference for inverse tag searches

--- lisp/ctags.el
+++ lisp/ctags.el
@@ -1,4 +1,4 @@
-;;; ctags.el --- an interface to ctags -*- lexical-binding: t; -*-
+;;; ctags.el --- an interface to Exuberant Ctags -*- lexical-binding: t; -*-
 
 ;; This is free and unencumbered software released into the public domain.
 
@@ -9,12 +9,13 @@
 ;; truly gigantic projects really need a "tags" database. Currently it
 ;; relies on Exuberant Ctags' "-R" option.
 
-;; This package provides one command: M-x `ctags-jump'. The suggested
+;; This package provides one command: M-x `ctags-find'. The suggested
 ;; binding:
 
-;;   (global-set-key (kbd "M-.") #'ctags-jump)
+;;   (global-set-key (kbd "M-.") #'ctags-find)
+;;   (global-set-key (kbd "M-?") #'ctags-find-reference)
 
-;; I haven'te bothered implementing a tag stack since I normally rely
+;; I haven't bothered implementing a tag stack since I normally rely
 ;; on Evil's jump list (C-o, C-i).
 
 ;;; Code:
@@ -27,7 +28,14 @@
 (defvar ctags-dominating-file "Makefile"
   "The file used to identify the root of the current project.")
 
-(defun ctags--extract-tag-info ()
+(defmacro ctags--with-temp-buffer (&rest body)
+  "Return the root"
+  (declare (indent defun))
+  `(let ((default-directory (locate-dominating-file "." ctags-dominating-file)))
+     (with-temp-buffer
+       ,@body)))
+
+(defun ctags--extract-tag ()
   "Extract file and line number information from the current line.
 This must only be called after a `re-search-forward' on the tag."
   (let* ((tag (match-string 0))
@@ -37,21 +45,17 @@ This must only be called after a `re-search-forward' on the tag."
                       (match-string 0))))
     (list tag (expand-file-name file default-directory) lineno)))
 
-(defun ctags-entries (&optional tag)
+(defun ctags--entries (&optional tag)
   "Return a list of all tag entries, or just the entries for TAG."
-  (let ((default-directory
-          (or (locate-dominating-file
-               default-directory ctags-dominating-file)
-              default-directory)))
-    (with-temp-buffer
-      (call-process ctags-program-name nil t nil "-Rx")
-      (setf (point) (point-min))
-      (cl-loop with regexp =
-               (if tag
-                   (format "^%s" (regexp-opt (list tag) 'symbols))
-                 "^\\S-+")
-               while (re-search-forward regexp nil t)
-               collect (ctags--extract-tag-info)))))
+  (ctags--with-temp-buffer
+    (call-process ctags-program-name nil t nil "-Rx")
+    (setf (point) (point-min))
+    (cl-loop with regexp =
+             (if tag
+                 (format "^%s" (regexp-opt (list tag) 'symbols))
+               "^\\S-+")
+             while (re-search-forward regexp nil t)
+             collect (ctags--extract-tag))))
 
 (defun ctags--visit (tag file n)
   "Switch to FILE, line number N, at TAG."
@@ -63,29 +67,61 @@ This must only be called after a `re-search-forward' on the tag."
     (when (re-search-forward regexp (line-end-position) t)
       (setf (point) (match-beginning 0)))))
 
-(defun ctags-jump (tag)
-  "Switch to buffer and line for TAG.
+(defun ctags--guess-tag (prefix)
+  "Try to guess what tag the user wants, or just ask."
+  ;; TODO: don't run ctags an extra time just for completion
+  (let ((tag (thing-at-point 'symbol)))
+    (if (or (null tag) prefix)
+        (let* ((tags (mapcar #'car (ctags--entries)))
+               (tag-valid-p (member tag tags))
+               (prompt (if tag-valid-p (format "Tag (%s): " tag) "Tag: ")))
+          (completing-read prompt tags nil t nil nil (if tag-valid-p tag)))
+      tag)))
 
-If already visiting this tag, go to the next instance of this tag.
+(defun ctags-find (tag)
+  "Switch to buffer and line for the next definition of TAG.
 
 When called interactively, TAG is the symbol under the point.
 Given a prefix argument, read TAG from the minibuffer."
-  ;; TODO: don't run ctags an extra time just for completion
-  (interactive
-   (let ((tag (thing-at-point 'symbol)))
-     (if (or (null tag) current-prefix-arg)
-         (let* ((tags (mapcar #'car (ctags-entries)))
-                (tag-valid-p (member tag tags))
-                (prompt (if tag-valid-p (format "Tag (%s): " tag) "Tag: ")))
-           (list (completing-read
-                  prompt tags nil t nil nil (if tag-valid-p tag))))
-       (list tag))))
+  (interactive (list (ctags--guess-tag current-prefix-arg)))
   (let* ((current (list tag buffer-file-name (line-number-at-pos)))
-         (entries (ctags-entries tag))
+         (entries (ctags--entries tag))
          (entry (car (or (cdr (member current entries))
                          entries))))
     (when entry
       (apply #'ctags--visit entry))))
+
+(defun ctags--grep-tag (tag entries)
+  "Return all references to TAG not included in ENTRIES."
+  (let ((files (cl-delete-duplicates (mapcar #'cadr entries) :test #'equal))
+        (regexp (format "\\<%s\\>" tag)))
+    (ctags--with-temp-buffer
+      (apply #'call-process "grep" nil t nil "-n" regexp "/dev/null" files)
+      (setf (point) (point-min))
+      (let ((refs ()))
+        (while (re-search-forward "^[^:]+" nil t)
+          (let* ((file (match-string 0))
+                 (lineno (progn (re-search-forward "[0-9]+")
+                                (string-to-number (match-string 0))))
+                 (ref (list tag file lineno)))
+            (unless (member ref entries)
+              (push ref refs))))
+        (nreverse refs)))))
+
+(defun ctags-find-reference (tag)
+  "Switch to buffer and line for the next reference to TAG.
+
+Tags are discovered using grep, and definitions are skipped in
+this search. When called interactively, TAG is the symbol under
+the point. Given a prefix argument, read TAG from the minibuffer."
+  (interactive (list (ctags--guess-tag current-prefix-arg)))
+  (let* ((current (list tag buffer-file-name (line-number-at-pos)))
+         (entries (ctags--entries tag))
+         (refs (ctags--grep-tag tag entries))
+         (ref (car (or (cdr (member current refs))
+                       refs))))
+    (when ref
+      (apply #'ctags--visit ref))))
 
 (provide 'ctags)
 
