fa24d5e0af81a0f893feb13c6ea1281289678ddc
Christopher Wellons <wellons@nullprogram.com>
2018-12-24 11:28:26 -0500

    elfeed: Add Ahoy (YouTube)

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -277,6 +277,7 @@
   ("PlumpHelmetPunk" youtube)
   ("UCAL3JXZSzSm8AlZyD3nQdBA" youtube) ; Primitive Technology
   ("ProZD" youtube)
+  ("XboxAhoy" youtube)
   ("ShamusYoung" youtube)
   ("UCO8DQrSp5yEP937qNqTooOw" youtube) ; Strange Parts
   ("UCy0tKL1T7wFoYcxCe0xjN6Q" youtube) ; Technology Connections
