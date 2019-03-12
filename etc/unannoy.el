0096ba4cd324c2d7f18a8f8cbf1fb6662bfbae08
Christopher Wellons <wellons@nullprogram.com>
2019-03-11 22:16:35 -0400

    Undo removal of indent-tabs-mode

--- etc/unannoy.el
+++ etc/unannoy.el
@@ -40,6 +40,9 @@
 (run-at-time 0 nil (lambda ()
 		     (setq-default lexical-binding t)))
 
+;; Tabs suck
+(setq-default indent-tabs-mode nil)
+
 ;; I hate typing
 (defalias 'yes-or-no-p 'y-or-n-p)
 
