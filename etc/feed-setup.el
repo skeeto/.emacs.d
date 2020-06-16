665b672067c56f3c092a2b4cef5ff0b210fdd9e4
Christopher Wellons <wellons@nullprogram.com>
2020-06-16 12:59:15 -0400

    elfeed: Add Stuart Wheaton

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -247,6 +247,7 @@
   ("https://www.schneier.com/blog/atom.xml" blog security)
   ("https://www.smbc-comics.com/comic/rss" comic)
   ("https://old.reddit.com/user/softwaring/submitted/.rss" image reddit)
+  ("https://stuartwheaton.com/feed.xml" blog dev)
   ("https://feeds.megaphone.fm/stuffyoushouldknow" audio)
   ("https://www.swordscomic.com/swords/feed/" comic)
   ("https://teokajlibroj.wordpress.com/feed/" blog esperanto)
