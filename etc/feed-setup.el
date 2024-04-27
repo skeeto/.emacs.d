59514e3e44edce3d20077752af94bc71a22acd95
Christopher Wellons <wellons@nullprogram.com>
2024-04-27 18:09:16 -0400

    elfeed: Add MaskRay

--- etc/feed-setup.el
+++ etc/feed-setup.el
@@ -221,6 +221,7 @@
   ("https://blogs.msdn.microsoft.com/oldnewthing/feed" blog dev)
   ("https://www.overcomingbias.com/feed" blog philosophy)
   ("http://feeds.feedburner.com/PoorlyDrawnLines" comic)
+  ("https://maskray.me/blog/atom.xml" blog dev)
   ("https://www.npr.org/rss/podcast.php?id=510289" podcast audio economics)
   ("https://possiblywrong.wordpress.com/feed/" blog math puzzle)
   ("http://feeds.wnyc.org/radiolab" audio)
